7be8e9cd6ecfea4e168f078d403bec0224896b27
BBOXEEEE <noeyhesx@naver.com>
2023-11-23 00:02:45 +0900

    initial commit

---
+++ MapReduceFramework/src/MapReduceFramework/MapReduceFramework.h
@@ -0,0 +1,21 @@
+/*
+ * @copyright 한국기술교육대학교 컴퓨터공학부 데이터베이스시스템
+ * @version 2023학년도 2학기
+ * @author 10조(김요셉, 박세현, 원현섭, 조승대)
+ * MapReduceFramework.h
+ */
+#pragma once
+#include "../MapReduceFramework/Mapper.h"
+#include "../MapReduceFramework/Reducer.h"
+
+#include <vector>
+
+class MapReduceFramework {
+public:
+    MapReduceFramework(Mapper* mapper, Reducer* reducer);
+    void run(const std::vector<std::string>& input, std::vector<std::pair<std::string, int>>& output);
+    // TODO : run 메소드의 인자 수정
+private:
+    Mapper* mapper;
+    Reducer* reducer;
+};
---
+++ MapReduceFramework/src/MapReduceFramework/Mapper.h
@@ -0,0 +1,20 @@
+/*
+ * @copyright 한국기술교육대학교 컴퓨터공학부 데이터베이스시스템
+ * @version 2023학년도 2학기
+ * @author 10조(김요셉, 박세현, 원현섭, 조승대)
+ * Mapper.h
+ */
+
+#ifndef MAPPER_H_
+#define MAPPER_H_
+#pragma once
+#include <string>
+#include <vector>
+
+class Mapper {
+public:
+	virtual ~Mapper() = default;
+	virtual void map(const std::string& input, std::vector<std::pair<std::string, int>>& output) = 0;
+};
+
+#endif /* MAPPER_H_ */
